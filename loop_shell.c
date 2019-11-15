697b1d7991bee20ea9594404da71d45c115b8d68
ultrasoft011 <ultrasoft.011@gmail.com>
2019-11-15 13:58:12 -0500

    loop

---
+++ loop_shell.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void loop_shell(void)
+{
+int status;
+char *commandLine;
+char **arguments;
+
+do {
+	printf("$ ");
+	commandLine = ();
+	// args = lsh_split_line(line);
+	// status = lsh_execute(args);
+
+free(line);
+}
+}
